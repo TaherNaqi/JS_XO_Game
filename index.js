f957fcbb85e20ef7f1bccb1fc9af1402717a1184
TaherNaqi <eng-naqi@hotmail.com>
2021-12-25 17:42:22 +0300

    XO first commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Premade function that will fill the button with its number.
 // First button top left is called 1, last button bottom right is 9
+// turn to alternate between X and O
+let turn = 0;
 function fillButton(index, text) {
   // This function fills
   document.getElementById(index).innerHTML = text;
@@ -7,8 +9,8 @@ function fillButton(index, text) {
 
 // SAMPLE CODE: This code fills the 1st and 9th button with X and O initially
 // Delete this code once you are done testing
-fillButton(1, "X");
-fillButton(9, "O");
+// fillButton(1, "X");
+// fillButton(9, "O");
 
 /**
  *
@@ -19,11 +21,21 @@ fillButton(9, "O");
  */
 function clickButton(index) {
   console.log(`Button number ${index} is clicked`);
+  fillButton(index, checkPlayer());
+  turn++;
+  checkWinner(index);
+  if (turn == 9) {
+    restartGame();
+  }
 }
 
 // in this function you should check if the player is X or O
+// ....
 function checkPlayer() {
-  // ....
+  if (turn % 2 == 0) {
+    return "X";
+  }
+  return "O";
 }
 
 /**
@@ -32,10 +44,86 @@ function checkPlayer() {
  * who is winning and returns the winner
  */
 // function checkWinner
-
+function checkWinner() {
+  //checking if X is winning
+  let r1 =
+    document.getElementById(1).innerHTML == "X" &&
+    document.getElementById(2).innerHTML == "X" &&
+    document.getElementById(3).innerHTML == "X";
+  let r2 =
+    document.getElementById(4).innerHTML == "X" &&
+    document.getElementById(5).innerHTML == "X" &&
+    document.getElementById(6).innerHTML == "X";
+  let r3 =
+    document.getElementById(7).innerHTML == "X" &&
+    document.getElementById(8).innerHTML == "X" &&
+    document.getElementById(9).innerHTML == "X";
+  let c1 =
+    document.getElementById(1).innerHTML == "X" &&
+    document.getElementById(4).innerHTML == "X" &&
+    document.getElementById(7).innerHTML == "X";
+  let c2 =
+    document.getElementById(2).innerHTML == "X" &&
+    document.getElementById(5).innerHTML == "X" &&
+    document.getElementById(8).innerHTML == "X";
+  let c3 =
+    document.getElementById(3).innerHTML == "X" &&
+    document.getElementById(6).innerHTML == "X" &&
+    document.getElementById(9).innerHTML == "X";
+  let d1 =
+    document.getElementById(1).innerHTML == "X" &&
+    document.getElementById(5).innerHTML == "X" &&
+    document.getElementById(9).innerHTML == "X";
+  let d2 =
+    document.getElementById(7).innerHTML == "X" &&
+    document.getElementById(5).innerHTML == "X" &&
+    document.getElementById(3).innerHTML == "X";
+  if (r1 | r2 | r3 | c1 | c2 | c3 | d1 | d2) {
+    winningAlert("X");
+  }
+  //Checking if O is winning
+  r1 =
+    document.getElementById(1).innerHTML == "O" &&
+    document.getElementById(2).innerHTML == "O" &&
+    document.getElementById(3).innerHTML == "O";
+  r2 =
+    document.getElementById(4).innerHTML == "O" &&
+    document.getElementById(5).innerHTML == "O" &&
+    document.getElementById(6).innerHTML == "O";
+  r3 =
+    document.getElementById(7).innerHTML == "O" &&
+    document.getElementById(8).innerHTML == "O" &&
+    document.getElementById(9).innerHTML == "O";
+  c1 =
+    document.getElementById(1).innerHTML == "O" &&
+    document.getElementById(4).innerHTML == "O" &&
+    document.getElementById(7).innerHTML == "O";
+  c2 =
+    document.getElementById(2).innerHTML == "O" &&
+    document.getElementById(5).innerHTML == "O" &&
+    document.getElementById(8).innerHTML == "O";
+  c3 =
+    document.getElementById(3).innerHTML == "O" &&
+    document.getElementById(6).innerHTML == "O" &&
+    document.getElementById(9).innerHTML == "O";
+  d1 =
+    document.getElementById(1).innerHTML == "O" &&
+    document.getElementById(5).innerHTML == "O" &&
+    document.getElementById(9).innerHTML == "O";
+  d2 =
+    document.getElementById(7).innerHTML == "O" &&
+    document.getElementById(5).innerHTML == "O" &&
+    document.getElementById(3).innerHTML == "O";
+  if (r1 | r2 | r3 | c1 | c2 | c3 | d1 | d2) {
+    winningAlert("O");
+  }
+}
 // function restart game
-
+function restartGame() {
+  location.reload();
+}
 // premade a function. You can use this function to present an alert to say somene wins
 function winningAlert(winner) {
   alert(`Horraaay, ${winner} wins!`);
+  location.reload();
 }
